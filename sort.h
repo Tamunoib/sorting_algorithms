6c4c81af50f7e954fb5c2a8c405e1425dc721142
Tamunoib <millerdavid0814@gmail.com>
2024-01-23 02:28:57 -0800

    0x1B.C sorting algorithm

--- sort.h
+++ sort.h
@@ -17,7 +17,7 @@ typedef enum bool
 {
 	false = 0.
 	true
-} bool:
+} bool;
 
 /**
  * struct listint_s - Doubly liked list node
@@ -29,9 +29,26 @@ typedef enum bool
 typedef struct listint_s
 {
 	const int n:
-	struct listint_s *prev:
-	struct listint_s *next:
-} listint_t:
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
 
 /* Printing helper functions */
-void print_array(const int *array, size_t size):
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+
+/* Sorting algorithms */
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif /* SORT_H */
